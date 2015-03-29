dc641206450e3d866fee8bd916e1f79b9dcd6511
Piotr Fryga <piotr.fryga@allegro.pl>
2015-03-29 22:42:49 +0200

    first

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 var express = require('express');
+var exModule = require('./models/module');
 var app = express();
 
 var port = 8080;
+var instanceModule = new exModule();
+
+instanceModule.init();
 
 app.listen(port, function () {
     console.log('server started on port ' + port);
---
+++ models/module.js
@@ -0,0 +1,9 @@
+module.exports = function () {
+    'use strict';
+
+    return {
+        init: function () {
+            console.log('Hello Module1');
+        }
+    }
+}
